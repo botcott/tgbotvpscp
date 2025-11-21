684b85e0957ee5cd9140451f4380c5217991ca29
Farid Shykhaliev <70578612+jatixs@users.noreply.github.com>
2025-11-21 12:48:19 +0300

    Refactor shared_state.py by cleaning up variables
    Removed unused variables and added AGENT_HISTORY list.

--- core/shared_state.py
+++ core/shared_state.py
@@ -7,16 +7,9 @@ LAST_MESSAGE_IDS = {}
 TRAFFIC_MESSAGE_IDS = {}
 ALERTS_CONFIG = {}
 USER_SETTINGS = {}
-
 NODES = {}
-
-# Словарь для мониторов трафика нод
 NODE_TRAFFIC_MONITORS = {}
-
-# --- НОВОЕ: Временные токены для Magic Link авторизации ---
-# Структура: {token: {"user_id": int, "created_at": float}}
 AUTH_TOKENS = {}
-# ----------------------------------------------------------
-
 RESOURCE_ALERT_STATE = {"cpu": False, "ram": False, "disk": False}
 LAST_RESOURCE_ALERT_TIME = {"cpu": 0, "ram": 0, "disk": 0}
+AGENT_HISTORY = []
